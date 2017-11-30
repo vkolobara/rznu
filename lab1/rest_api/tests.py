e3051df06634b89f1807ca2d2d80347f23a60edd
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-11-30 14:11:10 +0100

    User tests done

--- lab1/rest_api/tests.py
+++ lab1/rest_api/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
+from .models import *
+from .serializers import *
 
 # Create your tests here.
+
+class CreateUserTestCase(APITestCase):
+    def setUp(self):
+        self.data = {'username': 'bozo', 'password': 'bozo'}
+
+    def test_create_user(self):
+        response = self.client.post(reverse('user-list'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class ReadUserTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', password='bozo')
+
+    def test_read_user_list(self):
+        response = self.client.get(reverse('user-list'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['username'], self.user.username)
+
+    def tests_read_user_details(self):
+        response = self.client.get(reverse('user-detail', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
+    
+class UpdateUserTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', email='bozo@bozo.com', password='bozo')
+        self.data = {'username': 'bozo', 'password': 'bozo', 'email': 'superbozo@bozo.com'}
+        self.client.login(username=self.user.username, password='bozo')
+
+    def test_update_user(self):
+        response = self.client.put(reverse('user-detail', args=[self.user.id]), self.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], self.data['email'])
+
+class DeleteUserTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', email='bozo@bozo.com', password='bozo')
+        self.client.login(username=self.user.username, password='bozo')
+
+    def test_delete_user(self):
+        response = self.client.delete(reverse('user-detail', args=[self.user.id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
