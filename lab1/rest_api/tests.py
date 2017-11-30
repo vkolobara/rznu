6d9dd71731922230383e038a5b187353ca7d9f60
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-11-30 14:57:06 +0100

    Comment test cases

--- lab1/rest_api/tests.py
+++ lab1/rest_api/tests.py
@@ -13,7 +13,7 @@ class CreateUserTestCase(APITestCase):
 
     def test_create_user(self):
         response = self.client.post(reverse('user-list'), self.data)
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 class ReadUserTestCase(APITestCase):
     def setUp(self):
@@ -21,14 +21,14 @@ class ReadUserTestCase(APITestCase):
 
     def test_read_user_list(self):
         response = self.client.get(reverse('user-list'), format='json')
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(len(response.data), 1)
-        self.assertEquals(response.data[0]['username'], self.user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['username'], self.user.username)
 
     def tests_read_user_details(self):
         response = self.client.get(reverse('user-detail', args=[self.user.id]))
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data['username'], self.user.username)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
     
 class UpdateUserTestCase(APITestCase):
     def setUp(self):
@@ -38,8 +38,8 @@ class UpdateUserTestCase(APITestCase):
 
     def test_update_user(self):
         response = self.client.put(reverse('user-detail', args=[self.user.id]), self.data)
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data['email'], self.data['email'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], self.data['email'])
 
 class DeleteUserTestCase(APITestCase):
     def setUp(self):
@@ -48,7 +48,7 @@ class DeleteUserTestCase(APITestCase):
 
     def test_delete_user(self):
         response = self.client.delete(reverse('user-detail', args=[self.user.id]))
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 # Test posts
@@ -60,7 +60,7 @@ class CreatePostTestCase(APITestCase):
 
     def test_create_post(self):
         response = self.client.post(reverse('post-list'), self.data)
-        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 class ReadPostTestCase(APITestCase):
     def setUp(self):
@@ -70,14 +70,14 @@ class ReadPostTestCase(APITestCase):
     def test_read_post_list(self):
         response = self.client.get(reverse('post-list'))
 
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
 
     def test_read_post_details(self):
         response = self.client.get(reverse('post-detail', args=[self.post.id]))
 
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data['title'], self.post.title)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.post.title)
 
 class UpdatePostTestCase(APITestCase):
     def setUp(self):
@@ -90,8 +90,8 @@ class UpdatePostTestCase(APITestCase):
     def test_update_post(self):
         response = self.client.put(reverse('post-detail', args=[self.post.id]), self.data)
 
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertEquals(response.data['title'], self.data['title'])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.data['title'])
 
 class DeletePostTestCase(APITestCase):
     def setUp(self):
@@ -101,4 +101,81 @@ class DeletePostTestCase(APITestCase):
 
     def test_delete_post(self):
         response = self.client.delete(reverse('post-detail', args=[self.post.id]))
-        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+class CreateCommentTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', password='bozo')
+        self.post = Post.objects.create(author=self.user, title='title', text='text')
+        self.data = {'text':'text'}
+
+        self.client.login(username='bozo', password='bozo')
+
+    def test_create_comment(self):
+        response = self.client.post(reverse('comment-by-post-list', args=[self.post.id]), self.data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+class ReadCommentTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', password='bozo')
+        self.post = Post.objects.create(author=self.user, title='title', text='text')
+        self.comment = Comment.objects.create(author=self.user, post=self.post, text='text')
+
+        self.client.login(username='bozo', password='bozo')
+
+    def test_read_post_comment_list(self):
+        response = self.client.get(reverse('comment-by-post-list', args=[self.post.id]))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+    
+    def test_read_comment_list(self):
+        response = self.client.get(reverse('comment-list'))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_read_user_comment_list(self):
+        response = self.client.get(reverse('comment-by-user-list', args=[self.user.id]))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_read_comment_details(self):
+        response = self.client.get(reverse('comment-detail', args=[self.comment.id]))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['text'], self.comment.text)
+
+
+class UpdateCommentTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', password='bozo')
+        self.post = Post.objects.create(author=self.user, title='title', text='text')
+        self.comment = Comment.objects.create(author=self.user, post=self.post, text='text')
+
+        self.data = {'text': 'updated'}
+
+        self.client.login(username='bozo', password='bozo')
+
+    def test_update_comment(self):
+        response = self.client.put(reverse('comment-detail', args=[self.comment.id]), self.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['text'], self.data['text'])
+
+class DeleteCommentTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='bozo', password='bozo')
+        self.post = Post.objects.create(author=self.user, title='title', text='text')
+        self.comment = Comment.objects.create(author=self.user, post=self.post, text='text')
+
+        self.client.login(username='bozo', password='bozo')
+
+    def test_delete_comment(self):
+        response = self.client.delete(reverse('comment-detail', args=[self.comment.id]))
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
